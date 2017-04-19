08c2c4cb3a31de4c41d6ba73dbc0498e1dff0fb9
akloya <ajaykumar.loya@gmail.com>
2017-04-19 12:37:00 -0700

    Create EBSVolumesAlert.sh

---
+++ EBSVolumesAlert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script send alerts if you have ebs volumes with status "Available"
+
+MailTo=
+MailCc=
+
+# DO NOT EDIT ANY THING BELOW THIS LINE.
+echo "DevOps Team, Please plan to DELETE volumes with status: AVAILABLE" >ebsvolumes.log
+echo "" >>ebsvolumes.log
+aws ec2 describe-volumes --filters "Name=status,Values=available" --query Volumes[].[VolumeId,Size] --output=table --profile=buildbranch >>ebsvolumes.log
+COUNT=`cat ebsvolumes.log | grep -c "vol-" |awk '{print $1+1}'`
+if [ $COUNT == "1" ]; then
+echo "NOTHING TO DELETE....."
+else
+echo "PLAN TO DELETE ASAP for savings"
+mail -s 'ALERT! :UNUSED EBS VOLUMES FOUND IN LEANTAAS01 AWS ACCOUNT' $MailTo -c $MailCc  < ebsvolumes.log
+fi
