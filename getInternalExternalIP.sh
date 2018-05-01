ce1539e6bb4ab70f414faedcd9e96e60437833da
akloya <ajaykumar.loya@gmail.com>
2018-04-30 20:44:22 -0700

    Update getInternalExternalIP.sh

--- getInternalExternalIP.sh
+++ getInternalExternalIP.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Author: Ajaya Kumar Loya
+# This script will fetch you public and private IPs of instance.
+# Note this need be running on a Instance of which you need Instance ID.
 echo
 echo "Internal IPs:";
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
